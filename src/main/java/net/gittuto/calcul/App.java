5bd3722368dbc0c1a90ecbb881f33b07e29e8057
paped <paped@192.168.1.73>
2018-07-11 12:34:33 +0200

    next three

--- src/main/java/net/gittuto/calcul/App.java
+++ src/main/java/net/gittuto/calcul/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+    	System.out.println("monprojet");
+    	System.out.println("monautreprojet");
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!qqqqqdf" );
     }
